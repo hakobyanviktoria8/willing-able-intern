960b23720d26d61445542d4e8f9046171d71cbc6
Viktorya Hakobyan <hakobyanviktorya8@gmail.com>
2021-05-11 21:48:24 +0400

    web version

--- index.js
+++ index.js
@@ -1,4 +1,62 @@
-function openNav() {
-    document.getElementById("rightMenu").style.width = "250px";
+function openMenu() {
+    document.getElementById("rightMenu").style.width = "400px";
     console.log("here")
 }
+
+function closeMenu() {
+    document.getElementById("rightMenu").style.width = "0";
+}
+
+//Get the button
+let topBtn = document.getElementById("topBtn");
+
+window.onscroll = function() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        topBtn.style.display = "block";
+    } else {
+        topBtn.style.display = "none";
+    }
+};
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+//carusel slick
+$(document).ready(function () {
+    $('.slider').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows: true,
+        prevArrow : '<a href="" class="left_arrow"><i class="fas fa-chevron-left"></i></a>',
+        nextArrow : '<a href="" class="right_arrow"><i class="fas fa-chevron-right"></i></a>',
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+});
